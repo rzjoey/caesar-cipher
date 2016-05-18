6caca6f980b4c3b3b2db63955d240efff56203ba
Joseph Puchalski <sneidng@gmail.com>
2016-05-18 18:03:43 -0400

    improved cipher

--- caesarcipher.rb
+++ caesarcipher.rb
@@ -7,23 +7,24 @@ i = 0
 
 while i < string.length
 
-#HANDLES EMPTY SPACES
-if string[i] == " "
-	i+=1
-	encoded += " "
-	next
-end
-
-#HANDLES CAPITAL LETTERS
+#HANDLES DOWNCASE, UPCASE LETTERS, EMPTY SPACES, AND SYMBOLS
 if downcase_letters.include?(string[i]) == true
-			letters = downcase_letters
-else
-			letters = upcase_letters
+	letters = downcase_letters
+elsif upcase_letters.include?(string[i]) == true
+	letters = upcase_letters
+else 
+	encoded += string[i].to_s
+	i+=1
+	next	
 end
 
 	number = letters.index(string[i])
 
-#HANDLES SHIFTING PAST Z - WORKABLE SHIFTS INCLUDE 0-26
+#HANDLES SHIFTING PAST Z - WORKABLE SHIFTS INCLUDE 0-26, ADDED ERROR HANDLING FOR SHIFTS OVER 26
+	if shift > 26
+		shift = shift % 26
+	end
+
 	if number + shift > 25
 		number = number - 26
 	end
@@ -40,4 +41,4 @@ return encoded
 
 end
 
-puts cipher("rZjoey", 3)
+puts cipher("rZjoey!", 3)
